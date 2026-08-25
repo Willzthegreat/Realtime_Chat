cbd7ab9dd0aae3623bb27141a07f4c176da36dcf
Willzthegreat <obiadichinwuba@yahoo.com>
2026-08-25 09:52:08 +0100

    first commit

---
+++ realtime-chat/src/app/page.tsx
@@ -0,0 +1,34 @@
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle} from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
+import {  MessagesSquareIcon } from "lucide-react";
+import Link from "next/link"
+
+
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="container mx-auto max-w-3xl px-4 py-8 space-y-8">
+        <Empty className="border border-dashed">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <MessagesSquareIcon />
+            </EmptyMedia>
+            <EmptyTitle> 
+              No Chat Rooms
+            </EmptyTitle>
+            <EmptyDescription>
+              Create a new chat room to get started.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="rooms/new">Create a Chat Room</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    </>
+  )
+}
